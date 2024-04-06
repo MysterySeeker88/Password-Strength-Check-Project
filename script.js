fa680ff4e9c34f1bac0bc2cb995e7ab307c9eaa1
MysterySeeker88 <madk462@gmail.com>
2024-04-06 03:38:33 -0700

    JS changed

--- script.js
+++ script.js
@@ -1,28 +1,26 @@
 let message = document.querySelector('.message')
 let input = document.querySelector("input")
 let showHide = document.querySelector('#show')
+input.addEventListener('input', () => {
+    const passwordLength = input.value.length;
 
-input.addEventListener('input', (e) => {
-    if (input.value.length == 0) {
-        message.innerHTML = ''
+    if (passwordLength === 0) {
+        message.innerHTML = '';
+    } else if (passwordLength < 4) {
+        setMessage('Too weak', 'red');
+    } else if (passwordLength <= 5) {
+        setMessage('Weak', 'orange');
+    } else if (passwordLength < 8) {
+        setMessage('Medium', 'blue');
+    } else {
+        setMessage('Strong', 'green');
     }
-    else if (input.value.length < 4) {
-        message.innerHTML = 'Too weak'
-        message.style.color = 'red'
-    }
-    else if (input.value.length >= 4 && input.value.length <= 5) {
-        message.innerHTML = 'Weak'
-        message.style.color = 'orange'
-    }
-    else if (input.value.length > 5 && input.value.length < 8) {
-        message.innerHTML = 'Medium'
-        message.style.color = 'blue'
-    }
-    else if (input.value.length >= 8) {
-        message.innerHTML = 'Strong'
-        message.style.color = 'green'
-    }
-})
+});
+
+function setMessage(text, color) {
+    message.innerHTML = text;
+    message.style.color = color;
+}
 
 let t = true;
 showHide.addEventListener('click', function () {
